4d2dda3f7607f8dcf92bb05a4e76aec7fb006c0b
Kyle Harding <kyle@balena.io>
2022-02-03 14:45:31 -0500

    Fix authentication tests for private repos
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- tests/src/app.spec.ts
+++ tests/src/app.spec.ts
@@ -1,9 +1,12 @@
 import { expect } from 'chai';
-import { config, test } from '../../src/config';
+import { auth, config, test } from '../../src/config';
 import { parseReleaseRef } from '../../src/parse';
 import * as request from 'supertest';
 import { app } from '../..';
 
+const manifestSchema = 'application/vnd.docker.distribution.manifest.v2+json';
+const apiVersion = 'registry/2.0';
+const userAgent = 'docker/20.10.7';
 const releaseRef = parseReleaseRef(test.repository);
 const fleet = releaseRef?.fleet.slug;
 
@@ -24,18 +27,24 @@ const services = Array.from(
 	]),
 );
 
+const basicAuth = Buffer.from(`${auth.apiUsername}:${auth.apiToken}`).toString(
+	'base64',
+);
+
 releases.forEach((release) => {
 	services.forEach((service) => {
 		const repo = [fleet, release, release != null ? service : undefined]
 			.filter(Boolean)
 			.join('/');
 
-		let token = '';
+		let authToken = '';
+		// const identityToken = '';
 		// https://docs.docker.com/registry/spec/api/#api-version-check
 		describe('GET /v2/', function () {
 			it('responds with www-authenticate', async function () {
 				const response = await request(app)
 					.get('/v2/')
+					.set('User-Agent', userAgent)
 					.set('Accept', 'application/json');
 				// console.debug(response.headers);
 				// console.debug(response.body);
@@ -49,21 +58,23 @@ releases.forEach((release) => {
 		});
 
 		// https://docs.docker.com/registry/spec/auth/scope/
-		describe('GET /auth/v1/token/', function () {
+		describe('GET /auth/v1/token', function () {
 			it('responds with token', async function () {
 				const response = await request(app)
-					.get('/auth/v1/token/')
+					.get('/auth/v1/token')
 					.query({
+						account: auth.apiUsername,
 						scope: `repository:${repo}:pull`,
 						service: `${config.registryUrl.replace(/(^\w+:|^)\/\//, '')}`,
 					})
+					.set('Authorization', `Basic ${basicAuth}`)
 					.set('Accept', 'application/json');
 				// console.debug(response.headers);
 				// console.debug(response.body);
 				expect(response.status).equals(200);
 				expect(response.headers['content-type']).to.match(/application\/json/);
 				expect(response.body['token']).to.exist;
-				token = response.body['token'];
+				authToken = response.body['token'];
 			});
 		});
 
@@ -72,21 +83,17 @@ releases.forEach((release) => {
 			it('responds with manifest', async function () {
 				const response = await request(app)
 					.head(`/v2/${repo}/manifests/latest`)
-					.set('Authorization', `Bearer ${token}`)
-					.set(
-						'Accept',
-						'application/vnd.docker.distribution.manifest.v2+json',
-					);
+					.set('Authorization', `Bearer ${authToken}`)
+					.set('User-Agent', userAgent)
+					.set('Accept', manifestSchema);
 				// console.debug(response.headers);
 				// console.debug(response.body);
 				expect(response.status).equals(200);
 				expect(response.headers['docker-content-digest']).to.exist;
-				expect(response.headers['docker-distribution-api-version']).to.match(
-					/registry\/2.0/,
+				expect(response.headers['docker-distribution-api-version']).equals(
+					apiVersion,
 				);
-				expect(response.headers['content-type']).to.match(
-					/application\/vnd.docker.distribution.manifest.v2\+json/,
-				);
+				expect(response.headers['content-type']).equals(manifestSchema);
 			});
 		});
 	});
