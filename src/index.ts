94ffbc07c672efdd16e6bd72b52200e28a86d0a8
Kyle Harding <kyle@balena.io>
2022-03-15 11:22:12 -0400

    patch: Pass trust proxy from environment during init
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { REGISTRY_URL, PROXY_PORT } from './config';
+import { REGISTRY_URL, PROXY_PORT, TRUST_PROXY } from './config';
 import app from './app';
 
-const proxy = app(REGISTRY_URL);
+const proxy = app(REGISTRY_URL, TRUST_PROXY);
 
 proxy.listen(PROXY_PORT, () => {
 	console.log(`Proxy listening on port ${PROXY_PORT}`);
